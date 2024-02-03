ca5c018515f0f04a9f18b324b384dbaa530b7acb
5m44p1x <5m44p1x@gmail.com>
2024-02-03 14:06:04 -0600

    fix "linux/compiler-gcc11.h No such file or directory" issue

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -85,4 +85,7 @@
 #define __gcc_header(x) #x
 #define _gcc_header(x) __gcc_header(linux/compiler-gcc##x.h)
 #define gcc_header(x) _gcc_header(x)
-#include gcc_header(__GNUC__)
+// #include gcc_header(__GNUC__)
+#ifdef __GNUC__
+// #include <linux/compiler-gcc.h>
+#endif
